ce7255838d9eff69ba4714ca1adfd0f418e719c1
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2020-06-17 13:33:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,4 +9,4 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model  = GaborNN(200,16).to(device)
 
 
-summary(model,input_size=(7,7,200),batch_size=16))
+summary(model,input_size=(7,7,200),batch_size=16)
